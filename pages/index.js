8b5c3452cdd34078a3c27b5c91ca7d9699ec1067
Catalina Torrente <caatoba@gmail.com>
2022-04-13 18:06:09 -0500

    Link component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
+import Link from 'next/link';
+
 function HomePage(){
     return (
     <div>
         <h1>Página de Inicio </h1>
+        <ul>
+            <li>
+                <Link href="/portfolio">Portafolio</Link>
+            </li>
+            <li>
+                <Link href="/clients">Clientes</Link>
+            </li>
+            
+        </ul>
     </div>
     )
 }
