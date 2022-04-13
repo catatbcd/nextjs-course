20afe8976fe7587627bfa1cd6a8bbb6fb1bf917e
Catalina Torrente <caatoba@gmail.com>
2022-04-13 18:29:10 -0500

    navegacion programatica

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -4,9 +4,20 @@ function ClientProjectsPage(){
     const router = useRouter();
 
     console.log(router.query); 
+
+    function loadProjectHandler(){
+        //load data
+        //router.push('/clients/max/projecta');
+        router.push({
+            pathname:'/clients/[id]/[clientprojectid]',
+            query: { id: 'max', clientprojectid: 'projecta'},
+        })
+
+    }
     return (
     <div>
         <h1>Página de proyectos del cliente </h1>
+        <button onClick={loadProjectHandler}>Cargar proyecto A</button>
     </div>
     )
 }
