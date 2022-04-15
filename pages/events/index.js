8d6b1fe4301413407f920949d5b438f2f9068df1
Catalina Torrente <caatoba@gmail.com>
2022-04-14 19:06:03 -0500

    agregar busqueda

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,24 @@
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
+import { Fragment } from "react";
+import {useRouter} from 'next/router';
+
 function AllEventsPage(){
+    const router = useRouter();
     const events = getAllEvents();
+   
+    function findEventHandler(year, month){
+        const fullPath = `/events/${year}/${month}`;
+        router.push(fullPath);
+
+    }
 
     return (
-        <div>
+        <Fragment>
+            <EventsSearch onSearch={findEventHandler} />
             <EventList items={events}/>
-        </div>
+        </Fragment>
     )
 }
 export default AllEventsPage;
