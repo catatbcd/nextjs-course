97210b3df8cd594701ceb1d353b8feb3f421f445
Catalina Torrente <caatoba@gmail.com>
2022-04-14 23:34:05 -0500

    getStaticProps opciones de configuracion

--- pages/index.js
+++ pages/index.js
@@ -18,12 +18,26 @@ function HomePage(props) {
     const filePath = path.join(process.cwd(),'data','dummy-backend.json');
     const jsonData = await fs.readFile(filePath);
     const data = JSON.parse(jsonData);
+    if(!data){
+      return{
+        redirect: {
+          detination:'/no-data'
+
+        } //para redirigir
+      }
+    }
+
+    if(data.products.length === 0) {
+      return{ notFound:true // si es  true retorna 404 error
+    };
+    }
 
       return {
         props: {
           products: data.products
         },
-      revalidate: 10 // tiempo en seg para la siguiente regeneracion ISG (Incremental Static Generation)
+      revalidate: 10, // tiempo en seg para la siguiente regeneracion ISG (Incremental Static Generation)
+     
     };
 
 
