8a8ff6609b24446162b726bb6c735b6b35e311ba
Catalina Torrente <caatoba@gmail.com>
2022-04-15 12:59:01 -0500

    agregar srw

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,10 +1,30 @@
 import { useEffect, useState } from "react";
+import useSWR from 'swr';
 
 function LastSalesPage(){
     const [sales, setSales]= useState();
-    const[isLoading, setIsLoading] = useState(false);
+    //const[isLoading, setIsLoading] = useState(false);
 
+    const { data, error } = useSWR( 
+        'https://nextjs-course-97b0b-default-rtdb.firebaseio.com/sales.json'
+        );
+        console.log(data);
     useEffect(() => {
+        if(data) {
+            const transformedSales = [];
+
+            for (const key in data){
+                transformedSales.push({
+                    id:key, 
+                    username: data[key].username, 
+                    volume: data[key].volume,
+                });
+            }
+            
+            setSales(transformedSales);
+        }
+    },[data]);
+    /*useEffect(() => {
         setIsLoading(true);
         fetch(
             'https://nextjs-course-97b0b-default-rtdb.firebaseio.com/sales.json'
@@ -22,21 +42,27 @@ function LastSalesPage(){
             setSales(transformedSales);
             setIsLoading(false);
         });
-    },[])
+    },[])*/
 
-    if (isLoading){
-        return <p>Loading...</p>
+    //if (isLoading){
+    if(error){
+       return <p>Failed to load</p>
     }
-
+    if(!data){
+        return <p>!data Loading...</p>
+    }
     if(!sales){
-        return <p>No data yet</p>
+        return <p>!sales Loading...</p>
     }
-    return <ul>{sales.map((sale) =>(
-     <li key={sale.id} >
-         {sales.username}-${sale.volume}
-     </li>
-     ))}
-    </ul>;
+       
+    return (
+    <ul>
+        {sales.map((sale) =>(
+         <li key={sale.id} >
+            {sale.username}-${sale.volume}
+        </li>
+        ))}
+    </ul>);
 }
 
 export default LastSalesPage;
