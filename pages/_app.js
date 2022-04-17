f6195684902c8c427c4ee55ffbde04779bd7a875
Catalina Torrente <caatoba@gmail.com>
2022-04-16 22:50:49 -0500

    context

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,27 @@
-import Layout from '../components/layout/layout'
-import '../styles/globals.css'
-import Head from 'next/head';
+import Layout from "../components/layout/layout";
+import { NotificationContextProvider } from "../store/notification-context";
+import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
-  <Layout> 
-    <Head>
-    <title>Eventos NextJS</title>
-    <meta name="description" content="Encuentre una gran cantidad de eventos NextJs..." />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <Component {...pageProps} />
-  </Layout> 
+    <NotificationContextProvider>
+      <Layout>
+        <Head>
+          <title>Eventos NextJS</title>
+          <meta
+            name="description"
+            content="Encuentre una gran cantidad de eventos NextJs..."
+          />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
+        <Component {...pageProps} />
+      </Layout>
+    </NotificationContextProvider>
   );
-  }
-  
+}
+
 export default MyApp;
