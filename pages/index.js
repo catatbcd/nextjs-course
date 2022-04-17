24269decb7f51d26016f3217134ad0d0bb05913e
Catalina Torrente <caatoba@gmail.com>
2022-04-17 02:47:29 -0500

    metadata

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,18 @@ import { Fragment } from "react";
 import Hero from "../components/home/hero";
 import FeaturedPosts from "../components/home/featured-posts";
 import { getFeaturedPosts } from '../lib/posts-util';
-
+import Head from 'next/head';
 
 function HomePage(props) {
   return (
     <Fragment>
+      <Head>
+        <title>Max' Blog</title>
+        <meta
+          name='description'
+          content='I post about programming and web development.'
+        />
+      </Head>
       <Hero />
       <FeaturedPosts posts={props.posts} />
     </Fragment>
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,18 +1,30 @@
-import AllPosts from '../../components/posts/all-posts';
-import { getAllPosts } from '../../lib/posts-util';
+import AllPosts from "../../components/posts/all-posts";
+import { getAllPosts } from "../../lib/posts-util";
 
-
+import Head from "next/head";
+import { Fragment } from "react";
 
 function AllPostsPage(props) {
-  return <AllPosts posts={props.posts} />
+  return (
+    <Fragment>
+      <Head>
+        <title>All Posts</title>
+        <meta
+          name="description"
+          content="A list of all programming-related tutorials and posts!"
+        />
+      </Head>
+      <AllPosts posts={props.posts} />
+    </Fragment>
+  );
 }
 
 export function getStaticProps() {
   const allPosts = getAllPosts();
   return {
     props: {
-      posts: allPosts
-    }
+      posts: allPosts,
+    },
   };
 }
 export default AllPostsPage;
