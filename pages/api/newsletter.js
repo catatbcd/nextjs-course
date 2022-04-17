e7a59d1cfdd32b2e740dd91e25b654903eddf75e
Catalina Torrente <caatoba@gmail.com>
2022-04-16 20:26:15 -0500

    insertar email en mongoDB

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,14 +1,22 @@
-function handler(req, res){
-    if(req.method === 'POST'){
-        const userEmail = req.body.email;
+import { MongoClient } from "mongodb";
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const userEmail = req.body.email;
 
-        if(!userEmail || !userEmail.includes('@')){//validadcion de email basica
-            res.status(422).json({message: 'Direccion de correo incorrecta.'});
-            return;
-        }
-        console.log(userEmail);
-        res.status(201).json({message: 'Registrado'})
+    if (!userEmail || !userEmail.includes("@")) {
+      //validadcion de email basica
+      res.status(422).json({ message: "Direccion de correo incorrecta." });
+      return;
     }
+    const client = await MongoClient.connect(
+      "mongodb+srv://catat:vbq0U3LBYHi3iAr5@cluster0.nhdmh.mongodb.net/newsletter?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+    await db.collection('emails').insertOne({email:userEmail});
+    client.close();
+    console.log(userEmail);
+    res.status(201).json({ message: "Registrado" });
+  }
 }
 
 export default handler;
