091ffae9a24a090e9881d886ece4cc9aff3e704f
Catalina Torrente <caatoba@gmail.com>
2022-04-17 01:34:37 -0500

    convirtiendo markdown a jsx

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,25 @@
 import { Fragment } from "react";
 import Hero from "../components/home/hero";
 import FeaturedPosts from "../components/home/featured-posts";
-const DUMMY_POSTS = [
-    {
-      slug: 'getting-started-with-nextjs',
-      title: 'Getting Started with NextJS',
-      image: 'getting-started-nextjs.png',
-      excerpt:
-        'NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
-      date: '2022-02-10',
-    },
-    {
-      slug: 'getting-started-with-nextjs2',
-      title: 'Getting Started with NextJS',
-      image: 'getting-started-nextjs.png',
-      excerpt:
-        'NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
-      date: '2022-02-10',
-    },
-    {
-      slug: 'getting-started-with-nextjs3',
-      title: 'Getting Started with NextJS',
-      image: 'getting-started-nextjs.png',
-      excerpt:
-        'NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
-      date: '2022-02-10',
-    },
-    {
-      slug: 'getting-started-with-nextjs4',
-      title: 'Getting Started with NextJS',
-      image: 'getting-started-nextjs.png',
-      excerpt:
-        'NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
-      date: '2022-02-10',
-    },
-  ];
+import { getFeaturedPosts } from '../lib/posts-util';
 
-function HomePage() {
+
+function HomePage(props) {
   return (
     <Fragment>
       <Hero />
-      <FeaturedPosts posts={DUMMY_POSTS} />
+      <FeaturedPosts posts={props.posts} />
     </Fragment>
   );
 }
 
+export function getStaticProps() {
+    const featuredPosts = getFeaturedPosts();
+  
+    return {
+      props: {
+        posts: featuredPosts,
+      },
+    };
+  }
 export default HomePage;
