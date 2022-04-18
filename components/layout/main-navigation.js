960f0aae4b80dc95ca008867ee5e35305c4ec779
Catalina Torrente <caatoba@gmail.com>
2022-04-18 00:17:33 -0500

    cerrar sesion

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -1,9 +1,14 @@
 import Link from 'next/link';
-import { useSession} from 'next-auth/react';
+import { useSession, signOut } from 'next-auth/react';
 import classes from './main-navigation.module.css';
 
 function MainNavigation() {
-  const {session: session, loading:loading} = useSession();
+  const {data: session, status} = useSession();
+  const loading = status === "loading"
+
+  function logoutHandler() {
+    signOut();
+  }
   return (
     
     <header className={classes.header}>
@@ -26,7 +31,7 @@ function MainNavigation() {
           )}
           {session && (
             <li>
-              <button>Logout</button>
+              <button onClick={logoutHandler}>Logout</button>
             </li>
           )}
         </ul>
